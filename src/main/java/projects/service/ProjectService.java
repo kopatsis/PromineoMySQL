5315f952874826f1f5922ce2994f3910317486a7
Demetrios Kopatsis <jkopatsis@gmail.com>
2022-09-09 11:38:09 -0700

    wk11 commit

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectService {
 	
@@ -20,6 +21,18 @@ public class ProjectService {
 	public Project fetchProjectById(Integer projectId) {
 		return projectDao.fetchProjectById(projectId).orElseThrow(
 				() -> new NoSuchElementException("Project with project ID=" + projectId + " does not exist"));
+	}
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist");
+		}
+	}
+
+	public void deleteProject(Integer deleteId) {
+		if(!projectDao.deleteProject(deleteId)) {
+			throw new DbException("Project with ID=" + deleteId + " does not exist");
+		}
 	}
 
 }
